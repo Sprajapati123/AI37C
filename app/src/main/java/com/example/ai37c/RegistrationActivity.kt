216aa8e08b84a8ee8aabab7d0b4b3597079d3c86
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-17 15:24:04 +0545

    changes

---
+++ app/src/main/java/com/example/ai37c/RegistrationActivity.kt
@@ -0,0 +1,250 @@
+package com.example.ai37c
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ai37c.ui.theme.AI37CTheme
+import com.example.ai37c.ui.theme.Blue
+import com.example.ai37c.ui.theme.PurpleGrey80
+import com.example.ai37c.ui.theme.White
+import java.util.Calendar
+
+class RegistrationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RegisterBody()
+        }
+    }
+}
+@Composable
+fun RegisterBody(){
+    var email by remember { mutableStateOf("") }
+    var selectedDate by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var visibility by remember { mutableStateOf(false) }
+
+    var terms by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+
+    val calendar = Calendar.getInstance()
+
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    var datepicker = DatePickerDialog(
+        context,{
+            _,y,m,d->
+            selectedDate = "$y/${m+1}/$d"
+
+        },year,month,day
+    )
+
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(White)
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                "Sign Up",
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    color = Blue,
+                    fontSize = 24.sp
+                )
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+
+
+            OutlinedTextField(
+                value = email,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                onValueChange = { data ->
+                    email = data
+                },
+                placeholder = {
+                    Text("abc@gmail.com")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                shape = RoundedCornerShape(15.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = PurpleGrey80,
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                value = selectedDate,
+                onValueChange = {
+                    selectedDate = it
+                },
+                placeholder = {
+                    Text("dd/mm/yyyy")
+                },
+                enabled = false,
+                modifier = Modifier
+                    .fillMaxWidth().clickable{
+                        datepicker.show()
+                    }
+                    .padding(horizontal = 15.dp),
+                shape = RoundedCornerShape(15.dp),
+                colors = TextFieldDefaults.colors(
+                    disabledIndicatorColor = Color.Transparent,
+                    disabledContainerColor = PurpleGrey80,
+                    focusedContainerColor = PurpleGrey80,
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+
+
+            OutlinedTextField(
+                value = password,
+
+                onValueChange = {
+                    password = it
+                },
+                visualTransformation = if (visibility) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(onClick = {
+                        visibility = !visibility
+                    }) {
+                        Icon(
+                            painter = if (visibility)
+                                painterResource(R.drawable.baseline_visibility_off_24) else
+                                painterResource(R.drawable.baseline_visibility_24),
+                            contentDescription = null
+                        )
+
+                    }
+                },
+                placeholder = {
+                    Text("********")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                shape = RoundedCornerShape(15.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = PurpleGrey80,
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = terms,
+                    onCheckedChange = {
+                        terms = it
+                    },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Blue,
+                        checkmarkColor = White
+                    )
+                )
+                Text("I agree to terms & conditions")
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Blue
+                ),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp).padding(horizontal = 15.dp)
+            ) {
+                Text("Sign Up")
+            }
+
+            Text(buildAnnotatedString {
+                append("Already have account?")
+
+
+                withStyle(style = SpanStyle(color = Blue)){
+                    append(" Sign In")
+                }
+            }, style = TextStyle(fontSize = 16.sp), modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp))
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewRegister(){
+    RegisterBody()
+}
