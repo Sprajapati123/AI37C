515c44a7c4a9771fa209798fd48139d0fdef09e9
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-03 15:21:12 +0545

    nav bar

--- app/src/main/java/com/example/ai37c/DashboardActivity.kt
+++ app/src/main/java/com/example/ai37c/DashboardActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,11 +15,17 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -42,6 +49,22 @@ class DashboardActivity : ComponentActivity() {
 fun DashboardBody() {
     val context = LocalContext.current
     val activity = context as Activity
+
+    data class NavItem(val label:String,val icon: Int)
+
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    val listItem = listOf(
+        NavItem(label = "Home", icon = R.drawable.baseline_home_24),
+        NavItem(label = "Search", icon = R.drawable.baseline_search_24),
+        NavItem(label = "Notification", icon = R.drawable.baseline_notifications_24),
+        NavItem(label = "Profile", icon = R.drawable.baseline_person_24),
+    )
+
+
+
+
+
 
 //    val email = activity.intent.getStringExtra("email")
 //    val password = activity.intent.getStringExtra("password")
@@ -72,16 +95,36 @@ fun DashboardBody() {
                     }
                 }
             )
+        },
+        bottomBar = {
+            NavigationBar {
+                listItem.forEachIndexed { index,item->
+                    NavigationBarItem(
+                        icon = {
+                            Icon(painter = painterResource(item.icon),contentDescription = null)
+                        },
+                        label = {Text(item.label)},
+                        onClick = {
+                            selectedIndex =index
+                        },
+                        selected = selectedIndex == index
+                    )
+                }
+            }
         }
     ) { padding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
         ) {
-
-//            Text("Email: $email")
-//            Text("Password: $password")
+            when(selectedIndex){
+                0 -> HomeScreen()
+                1 -> SearchScreen()
+                2 -> NotificationScreen()
+                3 -> ProfileScreen()
+                else -> HomeScreen()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/ai37c/UserModel.kt
@@ -0,0 +1,3 @@
+package com.example.ai37c
+
+data class UserModel(val id:String)
