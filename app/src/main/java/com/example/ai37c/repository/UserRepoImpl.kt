b26f8a51635584b62b7c21ce4b789605118408d0
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-07 09:02:16 +0545

    firebase

--- app/src/main/java/com/example/ai37c/repository/UserRepoImpl.kt
+++ app/src/main/java/com/example/ai37c/repository/UserRepoImpl.kt
@@ -1,15 +1,34 @@
 package com.example.ai37c.repository
 
 import com.example.ai37c.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class UserRepoImpl : UserRepo {
+
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+    val ref: DatabaseReference = database.getReference("Users")
+
+
     override fun login(
         email: String,
         password: String,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    callback(true, "Login success")
+                } else {
+                    callback(false, "${it.exception?.message}")
+
+                }
+            }
     }
 
     override fun register(
@@ -17,7 +36,15 @@ class UserRepoImpl : UserRepo {
         password: String,
         callback: (Boolean, String, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    callback(true,"Registration sucess","${auth.currentUser?.uid}")
+                } else {
+                    callback(true,"${it.exception?.message}","")
+
+                }
+            }
     }
 
     override fun addUserToDatabase(
@@ -25,14 +52,20 @@ class UserRepoImpl : UserRepo {
         model: UserModel,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        ref.child(userId).setValue(model).addOnCompleteListener {
+            if(it.isSuccessful){
+                callback(true,"User registered successfully")
+            }else{
+                callback(false,"${it.exception?.message}")
+            }
+        }
     }
 
     override fun getUserById(
         userId: String,
         callback: (Boolean, UserModel) -> Unit
     ) {
-        TODO("Not yet implemented")
+       ref.child(userId).addValueEventListener()
     }
 
     override fun getAllUser(callback: (Boolean, List<UserModel>) -> Unit) {
